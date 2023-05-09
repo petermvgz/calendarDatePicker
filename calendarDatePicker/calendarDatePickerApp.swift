8675b6360e0b0682c9bc03791be22f0cf057bd06
Peter Deyi <peterdeyi@icloud.com>
2023-05-09 12:33:25 -0700

    Initial Commit

---
+++ calendarDatePicker/calendarDatePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  calendarDatePickerApp.swift
+//  calendarDatePicker
+//
+//  Created by Peter Deyi on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct calendarDatePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
