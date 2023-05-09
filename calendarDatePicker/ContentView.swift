6585ffd93fed2d22037e7f6f2af9c9689893f3a3
Peter Deyi <peterdeyi@icloud.com>
2023-05-09 13:09:58 -0700

    iOS calendar made in Building Business Applications

--- calendarDatePicker/ContentView.swift
+++ calendarDatePicker/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedDate = Date()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Text("Date: \(selectedDate.formatted(.dateTime.day().month().year()))")
+            // add a calendar to pick the date
+            DatePicker("Date:", selection: $selectedDate, displayedComponents: .date)
+                .labelsHidden()
+                .datePickerStyle(.graphical)
+                //.datePickerStyle:
+            // .automatic
+            // .compact
+            // .wheel
+            Spacer()
+                
         }
         .padding()
     }
